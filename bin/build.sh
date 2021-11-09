2ae849b1df10233b01022977f00c02804d3eb231
LOLO <154160595@qq.com>
2021-11-09 15:48:31 +0800

    first commit

---
+++ bin/build.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+path_sh=$(cd "$( dirname "$0")" && pwd)/
+cd ${path_sh}../lib
+node build.js
---
+++ bin/update.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+path_sh=$(cd "$( dirname "$0")" && pwd)/
+cd ${path_sh}../
+git pull origin main
